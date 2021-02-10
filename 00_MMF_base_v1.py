475bac72a9fe3a7cda760ec1da30de1399420597
bholaa72429 <bholaa72429@masseyhigh.school.nz>
2021-02-10 14:32:45 +1300

    todo

---
+++ 00_MMF_base_v1.py
@@ -0,0 +1,26 @@
+# import statement
+
+# function go here
+
+
+# ********** Main Routine **********
+
+# Set up dictionaries / lists needed to hold data
+
+# Ask the user if they have used the program before & show instruction if necessary
+
+# Loop to get ticket details
+
+    # Get name (can't be blank)
+
+    # Get age (between 12 and 130)
+
+    # Calculate ticket price
+
+    # Loop to ask for snacks
+
+    # calculate snack price
+
+    # ask for payment method (and apply surchange if necesary)
+
+# calculate total sales and profit
