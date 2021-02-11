cc8fcd0f629bf693b0bfc1cfc1f4ffd2bf2b89c3
bholaa72429 <bholaa72429@masseyhigh.school.nz>
2021-02-12 12:14:47 +1300

    ticket loop

---
+++ 02_ticket_loop_v1.py
@@ -0,0 +1,14 @@
+# start of loop
+
+# initialise loop so that it runs at least once
+name = ""
+count = 1
+MAX_TICKETS = 5
+
+while name != "xxx" and count <= MAX_TICKETS:
+
+    # Get details ...
+    name = input("Name: "
+                 "")
+    count += 1
+
