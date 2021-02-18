2e9c35d6eea639808fc96731acd2be8c1d43d3d9
bholaa72429 <bholaa72429@masseyhigh.school.nz>
2021-02-18 13:17:07 +1300

    - Integer Checker

---
+++ 03_number_checker.py
@@ -0,0 +1,24 @@
+# function goes here
+
+def int_check(question, low_num,high_num):
+
+    error = "Please enter a whole number between {} " \
+            "and {}".format(low_num, high_num)
+
+    valid = False
+    while not valid:
+        # ask user for a number and check its valid
+        try:
+            response = int(input(question))
+
+            if low_num < response < high_num:
+                return response
+            else:
+                print(error)
+
+        # If integer is not entered, show error
+        except ValueError:
+            print(error)
+
+# main routine goes here
+age = int_check("Age: ",12, 130)
