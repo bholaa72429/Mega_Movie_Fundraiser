f6c9912bb7f0dd4745ecc567b2f9d59923277284
bholaa72429 <58008527+bholaa72429@users.noreply.github.com>
2021-03-18 13:19:13 +1300

    MMF - Snack List Part 1 (1.10)

---
+++ 10_snack_list_expt_v2.py
@@ -0,0 +1,23 @@
+# Initialise snack lists...
+
+names = ['Alia', 'Rudra', 'Rahul', 'Sahil', 'Sid']
+
+popcorn = []
+mms = []
+pita_chips = []
+water = []
+orange_juice = []
+
+snack_lists = [popcorn,mms, pita_chips, water, orange_juice]
+
+snack_menu_dict = {
+    'Popcorn': popcorn,
+    'Water': water,
+    'Pita Chips': pita_chips,
+    'M&Ms': mms,
+    'Orange Juice': orange_juice
+}
+
+test_data = [
+
+]
