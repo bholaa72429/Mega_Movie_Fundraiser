10c229f79a5629ec0555545716e8f6329d1dff02
bholaa72429 <58008527+bholaa72429@users.noreply.github.com>
2021-04-12 10:46:55 +1200

    done

---
+++ 13_instructions.py
@@ -0,0 +1,51 @@
+# Function
+def string_check(choice, options):
+
+    is_valid = "yes"
+    chosen = ""
+    for var_list in options:
+
+        # if the snack  is in one of the lists, return the full
+        if choice in var_list:
+
+            # Get full name of snacks abd put it
+            # in title case so it looks nice when outputted
+            chosen = var_list[0].title()
+            is_valid = "yes"
+            break
+        # if the chosen option is not valid, set is_valid to no
+        else:
+            is_valid = "no"
+
+    # if the snack is not OK - ask question again.
+    if is_valid == "yes":
+        return chosen
+    else:
+        return "invalid choice"
+
+def instuctions(options):
+    show_help = "invalid choice"
+    while show_help == "invalid choice":
+        show_help = input("Would you like to read the instructions? ").lower()
+        show_help = string_check(show_help, options)
+
+    if show_help == "Yes":
+        print()
+        print("^^^^Mega Movie Fundraiser Instructions^^^^")
+        print()
+        print("--Welcome to MMF -- ")
+        print("This is a platform which allows you to purchases your movie tickets and pre-order your snacks")
+
+    return ""
+
+# ******** Main Routine *******
+# lists for valid options for yes / no response
+yes_no = [
+    ["yes", "y"],
+    ["no", "n"]
+]
+
+# Ask If instructions are needed
+instuctions(yes_no)
+print()
+print("Program Launches ...")
